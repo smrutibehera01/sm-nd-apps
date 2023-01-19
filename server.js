32820985befe16308a7396d9f24dd48277e92aa2
smruti ranjan behera <smrutibehera@kpmg.com>
2023-01-19 18:04:56 +0530

    19/01/2023 - learn creating request and send response

--- server.js
+++ server.js
@@ -2,7 +2,10 @@
 // ! Multiple variants to create server looks the code below
 
 // import statements 
- const http = require('http')
+ const { create } = require('domain');
+const http = require('http');
+const { url } = require('inspector');
+const fs = require('fs')
 
 //Create a function to listen the http.ServerResponse variant-1
 // function requestLq(req, res){
@@ -16,16 +19,56 @@
 //  });
 
 //variant- 3  Note --  Next Gen - of Es6 with arrow function to create server
-const server = http.createServer ((req, res) =>{
-    console.log(req);
-    //process.exit(); ! How to quit the event loop from the server
-});
+// const server = http.createServer ((req, res) => {
+//     res.write('Hello World!');
+//     res.end()
+//     console.log(req);
+//     //process.exit(); ! How to quit the event loop from the server
+// });
 
-server.listen(4000);
+// server.listen(2200);
 
 //Understanding Requests
 
-http.createServer ((req,res) =>{
-    //console.log('Server Configure Method', 'Methods : ' , req.method, 'Listen : ' , req.listen, 'Headers : ' , req.headers, 'URL : ' , req.url)
-    console.log(req.url, req.method, req.headers)
-}).listen(3000)
+// http.createServer ((req,res) =>{
+//     //console.log('Server Configure Method', 'Methods : ' , req.method, 'Listen : ' , req.listen, 'Headers : ' , req.headers, 'URL : ' , req.url)
+//     console.log(req.url, req.method, req.headers)
+// }).listen(3000)
+
+// e.g
+// http.createServer(function (req, res) {
+//     res.write('Hello World!'); //write a response to the client
+//     res.end(); //end the response
+//   }).listen(8080);
+
+// Date 19/23 Module is contuning 
+const serverlq = http.createServer ((req, res) =>{
+    console.log(req);
+    console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    const method = req.method
+    if(url === '/'){
+    // Create HTML content
+    res.setHeader('Content-type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Enter the Message!</title></head>');
+    res.write('<body><h1>Fill the Data in textbox then click Send !</h1><form action="/message" method="POST"><input name= "message" type="text"><button type="submit">Send</button></form></body>');
+    res.write('</html>')
+    return res.end()
+    }
+    if(url === '/message' && method == 'POST'){
+        fs.writeFileSync('message.txt', 'Hello world');
+        res.statusCode = 302;
+        res.setHeader('Location', '/')
+        return res.end()
+    }
+    // Create HTML 2 content
+    res.setHeader('Content-type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>First Node Apps !</title></head>');
+    res.write('<body><h1>Hello from Node JS Server</h1></body>');
+    res.write('</html>')
+    res.end()
+});
+serverlq.listen(8000);
+
